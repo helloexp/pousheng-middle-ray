c8a0e00f2a81b237ae2d546ebbd0d84804d20f8d
jlchen <jlchen.cn@gmail.com>
2017-06-02 19:02:03 +0800

    add miss bean configuration

--- middle-web/src/main/java/com/pousheng/middle/MiddleConfiguration.java
+++ middle-web/src/main/java/com/pousheng/middle/MiddleConfiguration.java
@@ -1,13 +1,19 @@
 package com.pousheng.middle;
 
 import com.pousheng.erp.ErpConfiguration;
+import com.pousheng.erp.component.SpuInfoFetcher;
+import com.pousheng.erp.model.PoushengMaterial;
 import io.terminus.parana.ItemApiConfiguration;
 import io.terminus.parana.ItemAutoConfig;
 //import io.terminus.parana.TradeApiConfig;
 //import io.terminus.parana.TradeAutoConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Description: add something here
@@ -22,5 +28,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         TradeAutoConfig.class*/})
 @EnableScheduling
 public class MiddleConfiguration {
+
+    @Bean
+    public SpuInfoFetcher<PoushengMaterial> spuInfoFetcher(){
+        return new SpuInfoFetcher<PoushengMaterial>() {
+            public List<PoushengMaterial> fetch(int pageNo, int pageSize, Date start, Date end) {
+                return null;
+            }
+        };
+    }
 
 }

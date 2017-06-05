ebcd65bd0ebb09a6a993c7fa6ea148fdd632096d
jlchen <jlchen.cn@gmail.com>
2017-06-05 09:24:39 +0800

    middle don't need delivery fee charge

--- middle-web/src/main/java/com/pousheng/middle/MiddleConfiguration.java
+++ middle-web/src/main/java/com/pousheng/middle/MiddleConfiguration.java
@@ -5,15 +5,20 @@ import com.pousheng.erp.component.SpuInfoFetcher;
 import com.pousheng.erp.model.PoushengMaterial;
 import io.terminus.parana.ItemApiConfiguration;
 import io.terminus.parana.ItemAutoConfig;
-//import io.terminus.parana.TradeApiConfig;
-//import io.terminus.parana.TradeAutoConfig;
+import io.terminus.parana.TradeApiConfig;
+import io.terminus.parana.TradeAutoConfig;
+import io.terminus.parana.order.api.DeliveryFeeCharger;
+import io.terminus.parana.order.dto.RichSkusByShop;
+import io.terminus.parana.order.model.ReceiverInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Description: add something here
@@ -24,8 +29,8 @@ import java.util.List;
 @Import({ItemAutoConfig.class,
         ErpConfiguration.class,
         ItemApiConfiguration.class,
-        /*TradeApiConfig.class,
-        TradeAutoConfig.class*/})
+        TradeApiConfig.class,
+        TradeAutoConfig.class})
 @EnableScheduling
 public class MiddleConfiguration {
 
@@ -33,7 +38,27 @@ public class MiddleConfiguration {
     public SpuInfoFetcher<PoushengMaterial> spuInfoFetcher(){
         return new SpuInfoFetcher<PoushengMaterial>() {
             public List<PoushengMaterial> fetch(int pageNo, int pageSize, Date start, Date end) {
+                //todo: implement me
                 return null;
+            }
+        };
+    }
+
+    /**
+     * 中台不需要计算运费
+     * @return deliveryFeeCharger
+     */
+    @Bean
+    public DeliveryFeeCharger deliveryFeeCharger(){
+        return new DeliveryFeeCharger() {
+            @Override
+            public Integer charge(Long aLong, Integer integer, Integer integer1) {
+                return 0;
+            }
+
+            @Override
+            public Map<Long, Integer> charge(List<RichSkusByShop> list, ReceiverInfo receiverInfo) {
+                return Collections.emptyMap();
             }
         };
     }

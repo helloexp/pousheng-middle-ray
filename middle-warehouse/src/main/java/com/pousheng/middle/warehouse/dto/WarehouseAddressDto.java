9574799c814232da02a29f3fc60ce715dbdb4ef7
jlchen <jlchen.cn@gmail.com>
2017-06-09 11:31:02 +0800

    refine warehouse address information

--- middle-warehouse/src/main/java/com/pousheng/middle/warehouse/dto/WarehouseAddressDto.java
+++ middle-warehouse/src/main/java/com/pousheng/middle/warehouse/dto/WarehouseAddressDto.java
@@ -1,6 +1,8 @@
 package com.pousheng.middle.warehouse.dto;
 
-import lombok.Data;
+import com.google.common.base.Objects;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
@@ -8,11 +10,35 @@ import java.io.Serializable;
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
  * Date: 2017-06-08
  */
-@Data
 public class WarehouseAddressDto implements Serializable{
     private static final long serialVersionUID = -8928998224540423389L;
 
+    @Getter
+    @Setter
     private String addressName;
 
+    @Getter
+    @Setter
     private Long addressId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WarehouseAddressDto that = (WarehouseAddressDto) o;
+        return Objects.equal(addressId, that.addressId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(addressId);
+    }
+
+    @Override
+    public String toString() {
+        return "WarehouseAddressDto{" +
+                "addressName='" + addressName + '\'' +
+                ", addressId=" + addressId +
+                '}';
+    }
 }
--- middle-web/src/main/java/com/pousheng/middle/warehouses/WarehouseAddressRules.java
+++ middle-web/src/main/java/com/pousheng/middle/warehouses/WarehouseAddressRules.java
@@ -1,8 +1,11 @@
 package com.pousheng.middle.warehouses;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.pousheng.middle.warehouse.cache.WarehouseAddressCacher;
 import com.pousheng.middle.warehouse.dto.WarehouseAddressDto;
 import com.pousheng.middle.warehouse.dto.WarehouseAddressTree;
+import com.pousheng.middle.warehouse.model.WarehouseAddress;
 import com.pousheng.middle.warehouse.service.WarehouseAddressRuleReadService;
 import com.pousheng.middle.warehouse.service.WarehouseAddressRuleWriteService;
 import com.pousheng.middle.warehouses.tree.TreeMarker;
@@ -15,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
@@ -38,18 +42,80 @@ public class WarehouseAddressRules {
     private TreeMarker treeMarker;
 
     /**
-     * 创建规则适用的地址信息, 返回rule id
+     * 创建规则适用的地址信息, 同时会创建仓库发货优先级规则, 并返回新创建的rule id
      *
      * @param addresses 对应的地址数组, 注意, 只需要将全选的节点提交上来即可, 部分选择和不选择的节点不要提交
      * @return rule id 新生成的规则id
      */
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Long create(@RequestBody WarehouseAddressDto[] addresses) {
-
-
-        return null;
+        //需要过滤掉本次提交中冗余的地址,如果父节点全选了, 那么子节点就可以过滤掉了
+        List<WarehouseAddressDto> valid = refineWarehouseAddress(addresses);
+        Response<Long> r = warehouseAddressRuleWriteService.batchCreate(valid);
+        if(!r.isSuccess()){
+            log.error("failed to create warehouse rule with addresses:{}, error code:{}", valid, r.getError());
+            throw new JsonResponseException(r.getError());
+        }
+        return r.getResult();
     }
 
+    /**
+     * 消除当前冗余的地址信息
+     *
+     * @param addresses 原始地址信息
+     * @return 过滤后的地址信息
+     */
+    private List<WarehouseAddressDto> refineWarehouseAddress(WarehouseAddressDto[] addresses) {
+        Set<Long> addressIds = Sets.newHashSetWithExpectedSize(addresses.length);
+        for (WarehouseAddressDto address : addresses) {
+            addressIds.add(address.getAddressId());
+        }
+        List<WarehouseAddressDto> valid = Lists.newArrayList();
+        for (WarehouseAddressDto address : addresses) {
+            Long addressId = address.getAddressId();
+            List<Long> ancestorIds = findAncestorIds(addressId);
+            boolean shouldIn = true;
+            for (Long ancestorId : ancestorIds) {
+                if(addressIds.contains(ancestorId)){
+                    shouldIn = false;
+                    break;
+                }
+            }
+            if(shouldIn){
+                valid.add(address);
+            }
+
+        }
+        return valid;
+    }
+
+    /**
+     * 查找当前地址节点的祖先id, 不包括自身id
+     *
+     * @param addressId 当前地址节点id
+     * @return 祖先id列表
+     */
+    private List<Long> findAncestorIds(Long addressId) {
+        WarehouseAddress current = warehouseAddressCacher.findById(addressId);
+        if(current == null){
+            log.error("WarehouseAddress(id={}) not exists", addressId);
+            throw new JsonResponseException("warehouseAddress.not.exists");
+        }
+        List<Long> parentIds = Lists.newArrayListWithExpectedSize(3);
+        while(current!=null && current.getPid()>0){
+            Long pid = current.getPid();
+            parentIds.add(pid);
+            current =  warehouseAddressCacher.findById(pid);
+        }
+        return parentIds;
+    }
+
+    /**
+     * 编辑规则时需要对应规则关联的发货地址信息
+     *
+     * @param ruleId 规则id
+     * @return  地址树形结构, 并已标记选中状态
+     */
     @RequestMapping(value = "/address", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public WarehouseAddressTree findAddressByRuleId(@RequestParam Long ruleId) {
         Response<List<WarehouseAddressDto>> r = warehouseAddressRuleReadService.findAddressByRuleId(ruleId);
@@ -57,6 +123,7 @@ public class WarehouseAddressRules {
             log.error("failed to find address for warehouse rule(id={}), error code:{}", ruleId, r.getError());
             throw new JsonResponseException(r.getError());
         }
+        //当前地址信息只到市一级, 因此, 树的深度为2
         WarehouseAddressTree warehouseAddressTree = warehouseAddressCacher.buildTree(2);
         List<WarehouseAddressDto> warehouseAddresses = r.getResult();
         for (WarehouseAddressDto warehouseAddress : warehouseAddresses) {

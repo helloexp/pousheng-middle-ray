9efeed427f7dff3b76198434a6fa7e383a364411
jlchen <jlchen.cn@gmail.com>
2017-07-05 15:39:12 +0800

    add job leader to avoid duplicate job execution

--- middle-web/src/main/java/com/pousheng/middle/web/erp/JobTriggers.java
+++ middle-web/src/main/java/com/pousheng/middle/web/erp/JobTriggers.java
@@ -2,6 +2,7 @@ package com.pousheng.middle.web.erp;
 
 import com.pousheng.erp.component.BrandImporter;
 import com.pousheng.erp.component.SpuImporter;
+import io.terminus.zookeeper.leader.HostLeader;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +23,15 @@ public class JobTriggers {
 
     private final BrandImporter brandImporter;
 
+    private final HostLeader hostLeader;
+
     @Autowired
-    public JobTriggers(SpuImporter spuImporter, BrandImporter brandImporter) {
+    public JobTriggers(SpuImporter spuImporter,
+                       BrandImporter brandImporter,
+                       HostLeader hostLeader) {
         this.spuImporter = spuImporter;
         this.brandImporter = brandImporter;
+        this.hostLeader = hostLeader;
     }
 
     /**
@@ -33,13 +39,17 @@ public class JobTriggers {
      */
     @Scheduled(cron="0 0 2 * * ?")
     public void synchronizeSpu(){
-        log.info("JOB -- begin to synchronize spus");
-        Date from = DateTime.now().minusDays(1).withTimeAtStartOfDay().toDate();
-        Date to = DateTime.now().withTimeAtStartOfDay().toDate();
-        int cardCount = brandImporter.process(from, to);
-        log.info("synchronized {} brands", cardCount);
-        int spuCount =spuImporter.process(from, to);
-        log.info("synchronized {} spus", spuCount);
-        log.info("JOB -- finish to synchronize spus");
+        if(hostLeader.isLeader()) {
+            log.info("JOB -- begin to synchronize spus");
+            Date from = DateTime.now().minusDays(1).withTimeAtStartOfDay().toDate();
+            Date to = DateTime.now().withTimeAtStartOfDay().toDate();
+            int cardCount = brandImporter.process(from, to);
+            log.info("synchronized {} brands", cardCount);
+            int spuCount = spuImporter.process(from, to);
+            log.info("synchronized {} spus", spuCount);
+            log.info("JOB -- finish to synchronize spus");
+        }else{
+            log.info("host is not leader, so skip job");
+        }
     }
 }

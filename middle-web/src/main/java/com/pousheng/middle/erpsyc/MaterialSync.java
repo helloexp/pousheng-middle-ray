1f4e5d1ed35e44d49e648442757fc63269bb2a74
songrenfei <songrenfei@terminus.io>
2017-07-12 16:29:08 +0800

    udpate profile

--- middle-web/src/main/java/com/pousheng/middle/erpsyc/MaterialSync.java
+++ middle-web/src/main/java/com/pousheng/middle/erpsyc/MaterialSync.java
@@ -26,7 +26,7 @@ import java.util.List;
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
  * Date: 2017-06-30
  */
-@Profile({"sync"})
+@Profile({"dev"})
 @Component
 @Slf4j
 public class MaterialSync implements CommandLineRunner {

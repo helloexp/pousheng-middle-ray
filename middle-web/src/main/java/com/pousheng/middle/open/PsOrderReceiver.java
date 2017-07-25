3b8f7058c21b272bf07b7f3f982c53f46ed892c3
chaopeng <gcp.socool@gmail.com>
2017-07-25 11:40:44 +0800

    find sku and item

---
+++ middle-web/src/main/java/com/pousheng/middle/open/PsOrderReceiver.java
@@ -0,0 +1,70 @@
+package com.pousheng.middle.open;
+
+import com.pousheng.middle.spu.service.PoushengMiddleSpuService;
+import io.terminus.boot.rpc.common.annotation.RpcConsumer;
+import io.terminus.common.model.Response;
+import io.terminus.open.client.center.job.order.component.DefaultOrderReceiver;
+import io.terminus.parana.item.model.Item;
+import io.terminus.parana.item.model.Sku;
+import io.terminus.parana.spu.model.SkuTemplate;
+import io.terminus.parana.spu.model.Spu;
+import io.terminus.parana.spu.service.SpuReadService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by cp on 7/25/17.
+ */
+@Component
+@Slf4j
+public class PsOrderReceiver extends DefaultOrderReceiver {
+
+    @RpcConsumer
+    private SpuReadService spuReadService;
+
+    @RpcConsumer
+    private PoushengMiddleSpuService middleSpuService;
+
+    @Override
+    protected Item findItemById(Long paranaItemId) {
+        //TODO use cache
+
+        Response<Spu> findR = spuReadService.findById(paranaItemId);
+        if (!findR.isSuccess()) {
+            log.error("fail to find spu by id={},cause:{}", paranaItemId, findR.getError());
+            return null;
+        }
+        Spu spu = findR.getResult();
+
+        Item item = new Item();
+        item.setId(spu.getId());
+        item.setName(spu.getName());
+        item.setMainImage(spu.getMainImage_());
+        return item;
+    }
+
+    @Override
+    protected Sku findSkuByCode(String skuCode) {
+        //TODO use cache
+        Response<SkuTemplate> findR = middleSpuService.findBySkuCode(skuCode);
+        if (!findR.isSuccess()) {
+            log.error("fail to find sku template by code={},cause:{}",
+                    skuCode, findR.getError());
+            return null;
+        }
+        SkuTemplate skuTemplate = findR.getResult();
+
+        Sku sku = new Sku();
+        sku.setId(skuTemplate.getId());
+        sku.setName(skuTemplate.getName());
+        sku.setPrice(skuTemplate.getPrice());
+        try {
+            sku.setExtraPrice(skuTemplate.getExtraPrice());
+        } catch (Exception e) {
+            //ignore
+        }
+        sku.setImage(skuTemplate.getImage_());
+        sku.setAttrs(skuTemplate.getAttrs());
+        return sku;
+    }
+}

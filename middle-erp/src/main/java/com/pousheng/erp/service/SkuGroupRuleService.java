e9f2a2a64f2ccd1994812e6f8f47353823e1d1ea
jlchen <jlchen.cn@gmail.com>
2017-07-31 15:08:16 +0800

    when create and update skuGroupRule, check card and kind duplicate

--- middle-erp/src/main/java/com/pousheng/erp/service/SkuGroupRuleService.java
+++ middle-erp/src/main/java/com/pousheng/erp/service/SkuGroupRuleService.java
@@ -1,5 +1,6 @@
 package com.pousheng.erp.service;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Throwables;
 import com.pousheng.erp.dao.mysql.SkuGroupRuleDao;
 import com.pousheng.erp.model.SkuGroupRule;
@@ -9,6 +10,7 @@ import io.terminus.common.model.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * Author:  <a href="mailto:i@terminus.io">jlchen</a>
@@ -25,32 +27,46 @@ public class SkuGroupRuleService {
         this.skuGroupRuleDao = skuGroupRuleDao;
     }
 
-    public Response<Long> create(SkuGroupRule skuGroupRule){
-        try{
+    public Response<Long> create(SkuGroupRule skuGroupRule) {
+        try {
+            //校验card和kind是否重复
+            SkuGroupRule existed = this.skuGroupRuleDao.findBYCardIdAndKindId(skuGroupRule.getCardId(),
+                    skuGroupRule.getKindId());
+            if (existed != null) {
+                log.error("duplicate card({}) and kind({})", skuGroupRule.getCardId(), skuGroupRule.getKindId());
+                return Response.fail("skuGroupRule.duplicate");
+            }
             skuGroupRuleDao.create(skuGroupRule);
             return Response.ok(skuGroupRule.getId());
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("failed to create {}, cause:{}", skuGroupRule, Throwables.getStackTraceAsString(e));
             return Response.fail("skuGroupRule.create.fail");
         }
     }
 
-    public Response<Boolean> update(SkuGroupRule skuGroupRule){
-        try{
+    public Response<Boolean> update(SkuGroupRule skuGroupRule) {
+        try {
             SkuGroupRule existed = skuGroupRuleDao.findById(skuGroupRule.getId());
-            if(existed == null){
+            if (existed == null) {
                 log.error("skuGroupRule(id={}) not exist", skuGroupRule.getId());
                 return Response.fail("skuGroupRule.update.fail");
             }
+            //校验card和kind是否重复
+            SkuGroupRule inDB = this.skuGroupRuleDao.findBYCardIdAndKindId(skuGroupRule.getCardId(),
+                    skuGroupRule.getKindId());
+            if (inDB != null && !Objects.equal(inDB.getId(), skuGroupRule.getId())) {
+                log.error("duplicate card({}) and kind({})", skuGroupRule.getCardId(), skuGroupRule.getKindId());
+                return Response.fail("skuGroupRule.duplicate");
+            }
             skuGroupRuleDao.update(skuGroupRule);
             return Response.ok(Boolean.TRUE);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("failed to create {}, cause:{}", skuGroupRule, Throwables.getStackTraceAsString(e));
             return Response.fail("skuGroupRule.create.fail");
         }
     }
 
-    public Response<Paging<SkuGroupRule>> findBy(Integer pageNo, Integer pageSize){
+    public Response<Paging<SkuGroupRule>> findBy(Integer pageNo, Integer pageSize) {
         try {
             PageInfo pageInfo = new PageInfo(pageNo, pageSize);
             return Response.ok(skuGroupRuleDao.paging(pageInfo.getOffset(), pageInfo.getLimit()));
@@ -60,9 +76,9 @@ public class SkuGroupRuleService {
         }
     }
 
-    public Response<SkuGroupRule> findById(Long id){
+    public Response<SkuGroupRule> findById(Long id) {
         SkuGroupRule skuGroupRule = skuGroupRuleDao.findById(id);
-        if(skuGroupRule == null){
+        if (skuGroupRule == null) {
             log.error("skuGroupRule(id={}) not found", id);
             return Response.fail("skuGroupRule.not.found");
         }

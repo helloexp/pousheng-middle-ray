7ff4ecc6da913c779f5aa6011208a1324d7493f6
songrenfei <songrenfei@terminus.io>
2017-12-08 13:50:46 +0800

    add setting discount

--- middle-item/src/main/java/com/pousheng/middle/item/impl/service/PsSkuTemplateWriteServiceImpl.java
+++ middle-item/src/main/java/com/pousheng/middle/item/impl/service/PsSkuTemplateWriteServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pousheng.middle.item.impl.service;
 
 import com.google.common.base.Throwables;
+import com.pousheng.middle.item.impl.dao.SkuTemplateExtDao;
 import com.pousheng.middle.item.service.PsSkuTemplateWriteService;
 import io.terminus.boot.rpc.common.annotation.RpcProvider;
 import io.terminus.common.model.Response;
@@ -9,6 +10,8 @@ import io.terminus.parana.spu.model.SkuTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by songrenfei on 2017/12/7
@@ -20,6 +23,8 @@ import org.springframework.stereotype.Component;
 public class PsSkuTemplateWriteServiceImpl implements PsSkuTemplateWriteService {
     @Autowired
     private SkuTemplateDao skuTemplateDao;
+    @Autowired
+    private SkuTemplateExtDao skuTemplateExtDao;
 
     @Override
     public Response<Boolean> update(SkuTemplate skuTemplate) {
@@ -35,6 +40,21 @@ public class PsSkuTemplateWriteServiceImpl implements PsSkuTemplateWriteService 
             return Response.ok(Boolean.TRUE);
         } catch (Exception e) {
             log.error("failed to update skuTemplate:{}, cause:{}", skuTemplate, Throwables.getStackTraceAsString(e));
+            return Response.fail("sku.template.update.fail");
+        }
+    }
+
+    @Override
+    public Response<Boolean> updateImageByIds(List<Long> ids, String imageUrl) {
+        try {
+            Boolean isUpdateSuccess = skuTemplateExtDao.updateImageByIds(ids,imageUrl);
+            if(isUpdateSuccess){
+                return Response.ok(isUpdateSuccess);
+            }else {
+                return Response.fail("sku.template.update.fail");
+            }
+        } catch (Exception e) {
+            log.error("failed to update skuTemplate:(ids:{}) image to:{}, cause:{}", ids,imageUrl, Throwables.getStackTraceAsString(e));
             return Response.fail("sku.template.update.fail");
         }
     }

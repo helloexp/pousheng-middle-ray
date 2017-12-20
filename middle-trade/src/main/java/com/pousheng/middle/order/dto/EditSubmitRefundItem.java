6debe343a30f97a5e8b545dbacecf544cd42b79e
zhaoxiaotao <zhaoxiaotao@terminus.io>
2017-12-20 09:17:59 +0800

    fixed some error

--- middle-trade/src/main/java/com/pousheng/middle/order/dto/EditSubmitRefundItem.java
+++ middle-trade/src/main/java/com/pousheng/middle/order/dto/EditSubmitRefundItem.java
@@ -9,6 +9,7 @@ import lombok.Data;
  */
 @Data
 public class EditSubmitRefundItem implements java.io.Serializable {
+    private static final long serialVersionUID = 5366346151064699012L;
     //商品编码和数量 (退货)
     private String refundSkuCode;
     //数量 (退货)

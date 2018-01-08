9341912b2f1631802d3630abf9f9cac8158d3e2b
zhaoxiaotao <zhaoxiaotao@terminus.io>
2018-01-08 17:09:13 +0800

    订单派发中心代码提交

---
+++ middle-web/src/main/java/com/pousheng/middle/yyedisyc/dto/YYEdiShipmentResponse.java
@@ -0,0 +1,22 @@
+package com.pousheng.middle.yyedisyc.dto;
+
+import com.pousheng.middle.yyedisyc.dto.trade.YYEdiShipmentResponseField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 同步发货单或者售后单时恒康返回消息的消息头
+ * Created by tony on 2017/7/26.
+ * pousheng-middle
+ */
+@Data
+public class YYEdiResponse implements Serializable{
+    private static final long serialVersionUID = -4030774668654863201L;
+    //200:整体成功,100:部分成功,-100:整体失败
+    private String errorCode;
+
+    private String description;
+    private List<YYEdiShipmentResponseField> fields;
+}
---
+++ middle-web/src/main/java/com/pousheng/middle/yyedisyc/dto/trade/YYEdiShipmentResponseField.java
@@ -0,0 +1,9 @@
+package com.pousheng.middle.yyedisyc.dto.trade;
+
+/**
+ * Author:  <a href="mailto:zhaoxiaotao@terminus.io">tony</a>
+ * Date: 2018/1/8
+ * pousheng-middle
+ */
+public class YYEdiShipmentResponseField {
+}

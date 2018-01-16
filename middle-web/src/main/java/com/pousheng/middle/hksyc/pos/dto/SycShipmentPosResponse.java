dc9ee89e94f4bba3619deb8bb7c5c7ebfdef6109
songrenfei <songrenfei@terminus.io>
2018-01-16 15:40:01 +0800

    fix param

---
+++ middle-web/src/main/java/com/pousheng/middle/hksyc/pos/dto/SycShipmentPosResponse.java
@@ -0,0 +1,16 @@
+package com.pousheng.middle.hksyc.pos.dto;
+
+import lombok.Data;
+
+/**
+ * Created by songrenfei on 2018/1/16
+ */
+@Data
+public class SycShipmentPosResponse {
+
+    private String code;
+
+    private String message;
+
+    private String returnJson;
+}

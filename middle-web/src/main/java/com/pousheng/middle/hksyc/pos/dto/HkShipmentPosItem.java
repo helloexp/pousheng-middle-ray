d55d4de2cca55e647c67cd10acda2cd5ec90bcb1
songrenfei <songrenfei@terminus.io>
2018-01-17 21:09:09 +0800

    fix sync error

--- middle-web/src/main/java/com/pousheng/middle/hksyc/pos/dto/HkShipmentPosItem.java
+++ middle-web/src/main/java/com/pousheng/middle/hksyc/pos/dto/HkShipmentPosItem.java
@@ -12,6 +12,8 @@ public class HkShipmentPosItem implements Serializable{
 
     private static final long serialVersionUID = -2142805920088639865L;
 
+    private String sourcenetbillno; //来源单号 for 退货
+    private String stockcode;  //仓库代码 for 退货
     private String matbarcode; //货号
     private Integer qty; //数量
     private String balaprice; //结算价

198cb492c2f76e5857b770c37c2ccb3f594f8118
songrenfei <songrenfei@terminus.io>
2018-01-18 11:35:17 +0800

    add item mapper

--- middle-web/src/main/java/com/pousheng/middle/web/item/component/PushMposItemComponent.java
+++ middle-web/src/main/java/com/pousheng/middle/web/item/component/PushMposItemComponent.java
@@ -4,12 +4,20 @@ import com.google.common.base.Function;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import io.terminus.common.model.Response;
 import io.terminus.common.utils.Joiners;
 import io.terminus.common.utils.JsonMapper;
 import io.terminus.open.client.center.item.dto.ParanaFullItem;
+import io.terminus.open.client.center.item.dto.ParanaSku;
+import io.terminus.open.client.center.item.dto.ParanaSkuAttribute;
 import io.terminus.open.client.center.item.service.ItemServiceCenter;
+import io.terminus.open.client.center.shop.OpenShopCacher;
+import io.terminus.open.client.common.mappings.model.ItemMapping;
+import io.terminus.open.client.common.mappings.service.MappingWriteService;
+import io.terminus.open.client.common.shop.model.OpenShop;
 import io.terminus.open.client.item.dto.ItemResult;
+import io.terminus.open.client.item.dto.OpenClientSkuAttribute;
 import io.terminus.open.client.parana.component.ParanaClient;
 import io.terminus.open.client.parana.dto.ParanaCallResult;
 import io.terminus.parana.spu.model.SkuTemplate;
@@ -17,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -38,6 +47,10 @@ public class PushMposItemComponent {
     private Long mposOpenShopId;
     @Autowired
     private ParanaClient paranaClient;
+    @Autowired
+    private OpenShopCacher openShopCacher;
+    @RpcConsumer
+    private MappingWriteService mappingWriteService;
     private static final JsonMapper mapper = JsonMapper.nonEmptyMapper();
 
 
@@ -49,7 +62,48 @@ public class PushMposItemComponent {
         Response<ItemResult> addR = itemServiceCenter.addItem(mposOpenShopId, paranaFullItem);
         if (!addR.isSuccess()) {
             log.error("fail to add item:{} for openShop(id={}),cause:{}", paranaFullItem, mposOpenShopId, addR.getError());
+        }else {
+            OpenShop openShop = openShopCacher.findById(mposOpenShopId);
+            Map<String, List<ParanaSkuAttribute>> paranaSkuAttributesBySkuCode = Maps.newHashMapWithExpectedSize(paranaFullItem.getSkus().size());
+            for (ParanaSku paranaSku : paranaFullItem.getSkus()) {
+                paranaSkuAttributesBySkuCode.put(paranaSku.getSkuCode(), paranaSku.getSkuAttributes());
+            }
+
+            ItemResult itemResult = addR.getResult();
+            List<ItemMapping> createdItemMappings = Lists.newArrayListWithCapacity(itemResult.getOpenSkuIds().size());
+            for (Map.Entry<String, String> entry : itemResult.getOpenSkuIds().entrySet()) {
+                ItemMapping itemMapping = new ItemMapping();
+                itemMapping.setChannel(openShop.getChannel());
+                itemMapping.setOpenShopId(openShop.getId());
+                itemMapping.setOpenShopName(openShop.getShopName());
+                itemMapping.setItemId(paranaFullItem.getItem().getItemId());
+                itemMapping.setItemName(paranaFullItem.getItem().getName());
+                itemMapping.setChannelItemId(itemResult.getOpenItemId());
+                itemMapping.setSkuCode(entry.getKey());
+                itemMapping.setChannelSkuId(entry.getValue());
+                itemMapping.setStatus(1);
+
+                List<ParanaSkuAttribute> paranaSkuAttributes = paranaSkuAttributesBySkuCode.get(entry.getKey());
+                if (!CollectionUtils.isEmpty(paranaSkuAttributes)) {
+                    List<OpenClientSkuAttribute> skuAttributes = Lists.newArrayListWithCapacity(paranaSkuAttributes.size());
+                    for (ParanaSkuAttribute paranaSkuAttribute : paranaSkuAttributes) {
+                        OpenClientSkuAttribute openClientSkuAttribute = new OpenClientSkuAttribute();
+                        openClientSkuAttribute.setAttributeKeyId(paranaSkuAttribute.getAttributeKeyId());
+                        openClientSkuAttribute.setAttributeKey(paranaSkuAttribute.getAttributeKey());
+                        openClientSkuAttribute.setAttributeValueId(paranaSkuAttribute.getAttributeValueId());
+                        openClientSkuAttribute.setAttributeValue(paranaSkuAttribute.getAttributeValue());
+                        skuAttributes.add(openClientSkuAttribute);
+                    }
+                    itemMapping.setSkuAttributes(skuAttributes);
+                }
+                createdItemMappings.add(itemMapping);
+            }
+            Response<Boolean> createItemMappingsR = mappingWriteService.createItemMappings(createdItemMappings);
+            if (!createItemMappingsR.isSuccess()) {
+                log.error("fail to create item mappings:{},cause:{}", createdItemMappings, createItemMappingsR.getError());
+            }
         }
+
     }
 
 

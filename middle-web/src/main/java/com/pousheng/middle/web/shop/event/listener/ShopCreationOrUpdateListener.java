3b670ed298b4a4aea4c7549c8446d90cd726b976
songrenfei <songrenfei@terminus.io>
2018-02-01 14:07:51 +0800

    add query shop address api

--- middle-web/src/main/java/com/pousheng/middle/web/shop/event/listener/ShopCreationOrUpdateListener.java
+++ middle-web/src/main/java/com/pousheng/middle/web/shop/event/listener/ShopCreationOrUpdateListener.java
@@ -1,26 +1,18 @@
 package com.pousheng.middle.web.shop.event.listener;
 
-import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.pousheng.erp.component.MposWarehousePusher;
 import com.pousheng.middle.gd.GDMapSearchService;
-import com.pousheng.middle.gd.Location;
-import com.pousheng.middle.order.dispatch.component.DispatchComponent;
 import com.pousheng.middle.order.enums.AddressBusinessType;
 import com.pousheng.middle.order.model.AddressGps;
 import com.pousheng.middle.order.service.AddressGpsReadService;
 import com.pousheng.middle.order.service.AddressGpsWriteService;
-import com.pousheng.middle.shop.enums.MemberFromType;
-import com.pousheng.middle.warehouse.cache.WarehouseAddressCacher;
-import com.pousheng.middle.warehouse.model.WarehouseAddress;
 import com.pousheng.middle.web.shop.component.MemberShopOperationLogic;
-import com.pousheng.middle.web.shop.dto.MemberCenterAddressDto;
 import com.pousheng.middle.web.shop.event.CreateShopEvent;
 import com.pousheng.middle.web.shop.event.UpdateShopEvent;
 import io.terminus.boot.rpc.common.annotation.RpcConsumer;
-import io.terminus.common.exception.ServiceException;
 import io.terminus.common.model.Response;
 import io.terminus.common.utils.Arguments;
 import io.terminus.open.client.parana.item.SyncParanaShopService;
@@ -28,7 +20,6 @@ import io.terminus.parana.cache.ShopCacher;
 import io.terminus.parana.shop.model.Shop;
 import io.terminus.parana.shop.service.ShopWriteService;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -44,8 +35,6 @@ public class ShopCreationOrUpdateListener {
     @Autowired
     private EventBus eventBus;
     @Autowired
-    private MemberShopOperationLogic memberShopOperationLogic;
-    @Autowired
     private GDMapSearchService gdMapSearchService;
     @RpcConsumer
     private AddressGpsWriteService addressGpsWriteService;
@@ -54,9 +43,7 @@ public class ShopCreationOrUpdateListener {
     @RpcConsumer
     private ShopWriteService shopWriteService;
     @Autowired
-    private WarehouseAddressCacher warehouseAddressCacher;
-    @Autowired
-    private DispatchComponent dispatchComponent;
+    private MemberShopOperationLogic memberShopOperationLogic;
     @Autowired
     private MposWarehousePusher mposWarehousePusher;
     @Autowired
@@ -73,7 +60,7 @@ public class ShopCreationOrUpdateListener {
     public void onCreated(CreateShopEvent event) {
        
 
-        AddressGps addressGps = getAddressGps(event.getShopId(),event.getCompanyId().toString(),event.getStoreCode());
+        AddressGps addressGps = memberShopOperationLogic.getAddressGps(event.getShopId(),event.getCompanyId().toString(),event.getStoreCode());
         if(Arguments.isNull(addressGps)){
             return;
         }
@@ -108,7 +95,7 @@ public class ShopCreationOrUpdateListener {
     @Subscribe
     public void onUpdate(UpdateShopEvent event) {
 
-        AddressGps addressGps = getAddressGps(event.getShopId(),event.getCompanyId().toString(),event.getStoreCode());
+        AddressGps addressGps = memberShopOperationLogic.getAddressGps(event.getShopId(),event.getCompanyId().toString(),event.getStoreCode());
         if(Arguments.isNull(addressGps)){
             return;
         }
@@ -147,92 +134,7 @@ public class ShopCreationOrUpdateListener {
             log.error("update shop:{} fail,error:{}",updateShop,updateShopRes.getError());
         }
     }
-    
-    private AddressGps getAddressGps(Long shopId,String companyId,String storeCode){
-        //1、调用会员中心查询门店地址
-        Response<MemberCenterAddressDto> addressDtoRes =  memberShopOperationLogic.findShopAddress(companyId,storeCode, MemberFromType.SHOP.value());
-        if(!addressDtoRes.isSuccess()){
-            log.error("find shop address by company id:{} code:{} type:{} fail,error:{}",companyId,storeCode,MemberFromType.SHOP.value(),addressDtoRes.getError());
-            return null;
-        }
-
-        MemberCenterAddressDto addressDto = addressDtoRes.getResult();
-        if(Arguments.isNull(addressDto)){
-            log.error("not find shop address by company id:{} code:{} type:{}",companyId,storeCode,MemberFromType.SHOP.value());
-            return null;
-        }
-        if(Strings.isNullOrEmpty(addressDto.getAddress())){
-            log.error("shop address is null for company id:{} code:{} type:{}",companyId,storeCode,MemberFromType.SHOP.value());
-            return null;
-        }
 
 
-        //2、调用高德地图查询地址坐标
-        Optional<Location> locationOp = dispatchComponent.getLocation(addressDto.getAddress());
-        if(!locationOp.isPresent()){
-            log.error("[ADDRESS-LOCATION]:not find shop(id:{}) location by address:{}",shopId,addressDto.getAddress());
-           return null;
-        }
-        Location location = locationOp.get();
 
-        //3、创建门店地址定位信息
-        AddressGps addressGps = new AddressGps();
-        addressGps.setLatitude(location.getLat());
-        addressGps.setLongitude(location.getLon());
-        addressGps.setBusinessId(shopId);
-        addressGps.setBusinessType(AddressBusinessType.SHOP.getValue());
-        addressGps.setDetail(addressDto.getAddress());
-        //省
-
-        log.info("[START-TRANS-ADDRESS] addressDto:{}",addressDto);
-        WarehouseAddress province = transToWarehouseAddress(addressDto.getProvinceCode(),addressDto.getProvinceName());
-        if(Arguments.isNull(province)){
-            return null;
-        }
-        addressGps.setProvinceId(province.getId());
-        addressGps.setProvince(province.getName());
-
-        //市
-        WarehouseAddress city = transToWarehouseAddress(addressDto.getCityCode(),addressDto.getCityName());
-        if(Arguments.isNull(city)){
-            return null;
-        }
-        addressGps.setCityId(city.getId());
-        addressGps.setCity(city.getName());
-
-        //区
-        WarehouseAddress region = transToWarehouseAddress(addressDto.getAreaCode(),addressDto.getAreaName());
-        if(Arguments.isNull(region)){
-            return null;
-        }
-        addressGps.setRegionId(region.getId());
-        addressGps.setRegion(region.getName());
-
-        return addressGps;
-    }
-
-
-    //将会员中心的地址与中台地址做比较，转换为中台的地址
-    private WarehouseAddress transToWarehouseAddress(String code,String name){
-        try {
-            if(Strings.isNullOrEmpty(code)){
-                log.error("[QUERY-MIDDLE-ADDRESS] code is null",code,name);
-                return null;
-            }
-            Long addressId = Long.valueOf(code);
-            WarehouseAddress address = warehouseAddressCacher.findById(addressId);
-            if(Arguments.isNull(address)){
-                log.error("[QUERY-MIDDLE-ADDRESS] by code:{} name:{} not find",code,name);
-                return null;
-            }
-            return address;
-        }catch (ServiceException e){
-            log.error("check is matching warehouse address by code:{} name:{} fail,error:{}",code,name, e.getMessage());
-            return null;
-        }catch (Exception e){
-            log.error("check is matching warehouse address by code:{} name:{} fail,cause:{}",code,name, Throwables.getStackTraceAsString(e));
-            return null;
-        }
-
-    }
 }

e078f7712d6ca4adf85f0e5ac8080bc20767cb48
songrenfei <songrenfei@terminus.io>
2018-04-06 23:20:08 +0800

    fix stock query error

--- middle-web/src/main/java/com/pousheng/middle/web/warehouses/component/WarehouseSkuStockLogic.java
+++ middle-web/src/main/java/com/pousheng/middle/web/warehouses/component/WarehouseSkuStockLogic.java
@@ -1,7 +1,9 @@
 package com.pousheng.middle.web.warehouses.component;
 
-import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.pousheng.middle.hksyc.component.QueryHkWarhouseOrShopStockApi;
+import com.pousheng.middle.hksyc.dto.item.HkSkuStockInfo;
 import com.pousheng.middle.order.dispatch.component.DispatchComponent;
 import com.pousheng.middle.shop.cacher.MiddleShopCacher;
 import com.pousheng.middle.shop.dto.ShopExtraInfo;
@@ -12,13 +14,18 @@ import com.pousheng.middle.warehouse.service.WarehouseSkuReadService;
 import io.terminus.common.exception.ServiceException;
 import io.terminus.common.model.Response;
 import io.terminus.common.utils.Arguments;
+import io.terminus.common.utils.Splitters;
 import io.terminus.parana.shop.model.Shop;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by songrenfei on 2018/3/27
@@ -35,24 +42,36 @@ public class WarehouseSkuStockLogic {
     private MiddleShopCacher middleShopCacher;
     @Autowired
     private DispatchComponent dispatchComponent;
+    @Autowired
+    private QueryHkWarhouseOrShopStockApi queryHkWarhouseOrShopStockApi;
 
 
     public Response<Map<String,Integer>> findByWarehouseIdAndSkuCodes(Long warehouseId, List<String> skuCodes){
 
+        Warehouse warehouse = warehouseCacher.findById(warehouseId);
+        Map<String, String> extra = warehouse.getExtra();
+        if(!extra.containsKey("outCode")){
+            log.error("warehouse(id:{}) out code is null,so skip to count stock");
+            return Response.fail("warehouse.out.code.invalid");
+        }
+
         Response<List<WarehouseSkuStock>> listRes = warehouseSkuReadService.findSkuStocks(warehouseId,skuCodes);
         List<WarehouseSkuStock> stocks = listRes.getResult();
-        Map<String, Integer> r = Maps.newHashMapWithExpectedSize(skuCodes.size());
-        for (WarehouseSkuStock stock : stocks) {
 
-            Warehouse warehouse = warehouseCacher.findById(stock.getWarehouseId());
-            Map<String, String> extra = warehouse.getExtra();
-            if(!extra.containsKey("outCode")){
-                log.error("warehouse(id:{}) out code is null,so skip to count stock");
-                continue;
+        Map<String, WarehouseSkuStock> skuCodeMap = stocks.stream().filter(Objects::nonNull)
+                .collect(Collectors.toMap(WarehouseSkuStock::getSkuCode, it -> it));
+
+        Map<String, Integer> r = Maps.newHashMapWithExpectedSize(skuCodes.size());
+        for (String skuCode : skuCodes) {
+
+
+            WarehouseSkuStock stock =null;
+            if(skuCodeMap.containsKey(skuCode)){
+                stock = skuCodeMap.get(skuCode);
             }
 
             //如果是店仓则要减掉中台的占用库存
-            if(Objects.equal(warehouse.getType(),1)){
+            if(Objects.equals(warehouse.getType(),1)){
                 String outerId = extra.get("outCode");
                 String companyId = warehouse.getCompanyId();
                 Shop shop = middleShopCacher.findByOuterIdAndBusinessId(outerId,Long.valueOf(companyId));
@@ -61,16 +80,28 @@ public class WarehouseSkuStockLogic {
                     log.error("not find shop(id:{}) extra info by shop extra info json:{} ",shop.getId(),shop.getExtra());
                     throw new ServiceException("shop.safe.stock.invalid");
                 }
+
+                List<HkSkuStockInfo> hkSkuStockInfos = queryHkWarhouseOrShopStockApi.doQueryStockInfo(Lists.newArrayList(outerId),Lists.newArrayList(skuCode),0);
+                if(CollectionUtils.isEmpty(hkSkuStockInfos)){
+                    log.error("not query stock from hk where stock code:{} sku code:{}",outerId,skuCode);
+                    continue;
+                }
+                //可用库存
+                Integer hkAvailStock = hkSkuStockInfos.get(0).getMaterial_list().get(0).getQuantity();
                 //安全库存
                 Integer safeStock = Arguments.isNull(shopExtraInfo.getSafeStock())?0:shopExtraInfo.getSafeStock();
-                Long localStock =  dispatchComponent.getMposSkuShopLockStock(shop.getId(),stock.getSkuCode());
-                Long availStock = stock.getAvailStock() - localStock - safeStock;
+                Long localStock =  dispatchComponent.getMposSkuShopLockStock(shop.getId(),skuCode);
+                Long availStock = hkAvailStock - localStock - safeStock;
                 if(availStock<=0L){
                     availStock =0L;
                 }
-                r.put(stock.getSkuCode(), availStock.intValue());
+                r.put(skuCode, availStock.intValue());
             }else {
-                r.put(stock.getSkuCode(), stock.getAvailStock().intValue());
+                if(Arguments.isNull(stock)){
+                    log.error("not find stock by warehouse id:{} sku code:{}",warehouseId,skuCode);
+                    continue;
+                }
+                r.put(skuCode, stock.getAvailStock().intValue());
             }
         }
         return Response.ok(r);

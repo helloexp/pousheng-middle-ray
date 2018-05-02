649fedbe9b6a73809b364859ec9ffca509a39bea
songrenfei <songrenfei@terminus.io>
2018-05-03 00:34:40 +0800

    add sku is mpos check

--- middle-web/src/main/java/com/pousheng/middle/hksyc/component/QueryHkWarhouseOrShopStockApi.java
+++ middle-web/src/main/java/com/pousheng/middle/hksyc/component/QueryHkWarhouseOrShopStockApi.java
@@ -1,6 +1,7 @@
 package com.pousheng.middle.hksyc.component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.base.Function;
 import com.google.common.base.Objects;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
@@ -11,16 +12,21 @@ import com.pousheng.middle.shop.cacher.MiddleShopCacher;
 import com.pousheng.middle.warehouse.cache.WarehouseCacher;
 import com.pousheng.middle.warehouse.model.Warehouse;
 import com.pousheng.middle.warehouse.service.WarehouseReadService;
+import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import io.terminus.common.exception.ServiceException;
+import io.terminus.common.model.Response;
 import io.terminus.common.utils.Joiners;
 import io.terminus.common.utils.JsonMapper;
 import io.terminus.parana.shop.model.Shop;
+import io.terminus.parana.spu.model.SkuTemplate;
+import io.terminus.parana.spu.service.SkuTemplateReadService;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +49,8 @@ public class QueryHkWarhouseOrShopStockApi {
     private WarehouseCacher warehouseCacher;
     @Autowired
     private WarehouseReadService warehouseReadService;
+    @RpcConsumer
+    private SkuTemplateReadService skuTemplateReadService;
 
 
     private static final TypeReference<List<HkSkuStockInfo>> LIST_OF_SKU_STOCK = new TypeReference<List<HkSkuStockInfo>>() {};
@@ -95,7 +103,10 @@ public class QueryHkWarhouseOrShopStockApi {
                     }
                     skuStockInfo.setBusinessId(shop.getId());
                     skuStockInfo.setBusinessName(shop.getName());
-                    middleStockList.add(skuStockInfo);
+                    //商品必须打标为mpos标签才可以参与门店发货
+                    if(checkIsMposSku(skuStockInfo)){
+                        middleStockList.add(skuStockInfo);
+                    }
 
                 }catch (Exception e){
                     log.error("find shop by outer id:{} fail,cause:{}",skuStockInfo.getStock_code(),Throwables.getStackTraceAsString(e));
@@ -107,6 +118,34 @@ public class QueryHkWarhouseOrShopStockApi {
         return middleStockList;
     }
 
+
+    private Boolean checkIsMposSku(HkSkuStockInfo skuStockInfo){
+
+        List<String> skuCodes = Lists.transform(skuStockInfo.getMaterial_list(), new Function<HkSkuStockInfo.SkuAndQuantityInfo, String>() {
+            @Nullable
+            @Override
+            public String apply(@Nullable HkSkuStockInfo.SkuAndQuantityInfo input) {
+                return input.getBarcode();
+            }
+        });
+
+        Response<List<SkuTemplate>> listRes = skuTemplateReadService.findBySkuCodes(skuCodes);
+        if(!listRes.isSuccess()){
+            log.error("find sku template by sku codes:{} fail,error:{}",skuCodes,listRes.getError());
+            //这里将查询失败当做非mpos商品处理（严格意义上这里应该要报错的）
+            return Boolean.FALSE;
+        }
+        List<SkuTemplate> skuTemplates = listRes.getResult();
+
+        if(CollectionUtils.isEmpty(skuTemplates)){
+            log.error("not find sku template by sku codes:{} ",skuCodes);
+            //这里将查询不到的当做非mpos商品处理（严格意义上这里应该要报错的）
+            return Boolean.FALSE;
+        }
+
+        return Objects.equal(skuTemplates.get(0).getType(),2);
+
+    }
 
 
 

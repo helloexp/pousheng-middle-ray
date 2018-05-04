cee6f7db18873bc3645fba874de86e03032104d5
zhaoxiaotao <zhaoxiaotao@terminus.io>
2018-05-04 14:29:45 +0800

    库存同步日志批量插入

--- middle-warehouse/src/main/java/com/pousheng/middle/warehouse/service/MiddleStockPushLogWriteService.java
+++ middle-warehouse/src/main/java/com/pousheng/middle/warehouse/service/MiddleStockPushLogWriteService.java
@@ -3,6 +3,8 @@ package com.pousheng.middle.warehouse.service;
 import com.pousheng.middle.warehouse.model.StockPushLog;
 import io.terminus.common.model.Response;
 
+import java.util.List;
+
 /**
  * Author:  <a href="mailto:zhaoxiaotao@terminus.io">tony</a>
  * Date: 2017/11/10
@@ -12,8 +14,17 @@ public interface MiddleStockPushLogWriteService {
 
     /**
      * 创建stockPushLog
+     *
      * @return 主键id
      */
     Response<Long> create(StockPushLog stockPushLog);
+
+    /**
+     * 批量创建stockPushLog
+     *
+     * @param stockPushLogs
+     * @return
+     */
+    Response<Boolean> creates(List<StockPushLog> stockPushLogs);
 
 }

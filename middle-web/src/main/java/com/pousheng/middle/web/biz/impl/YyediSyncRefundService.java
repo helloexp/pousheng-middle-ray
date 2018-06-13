1b99331907a4f5ca1d35cbee35703ca1b04b5ac3
zhaoxiaotao <zhaoxiaotao@terminus.io>
2018-06-13 11:30:39 +0800

    退货单退货处理任务添加日志

--- middle-web/src/main/java/com/pousheng/middle/web/biz/impl/YyediSyncRefundService.java
+++ middle-web/src/main/java/com/pousheng/middle/web/biz/impl/YyediSyncRefundService.java
@@ -63,6 +63,7 @@ public class YyediSyncRefundService implements CompensateBizService {
             log.warn("YyediSyncRefundService.doProcess shipInfos is null");
             return;
         }
+        log.info("yyedi sync refund service ,refunds size is {}",refunds.size());
         refunds.stream().forEach(a -> {
             //退货单编码
             String refundCode = a.getRefundCode();

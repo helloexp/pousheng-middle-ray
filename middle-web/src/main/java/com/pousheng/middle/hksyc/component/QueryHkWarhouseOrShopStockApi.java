695e37986d40ae4e26e151d4b8e5818bcc526602
songrenfei <songrenfei@terminus.io>
2018-06-15 10:30:37 +0800

    update is mpos check

--- middle-web/src/main/java/com/pousheng/middle/hksyc/component/QueryHkWarhouseOrShopStockApi.java
+++ middle-web/src/main/java/com/pousheng/middle/hksyc/component/QueryHkWarhouseOrShopStockApi.java
@@ -2,7 +2,6 @@ package com.pousheng.middle.hksyc.component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.base.Function;
-import com.google.common.base.Objects;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -15,6 +14,7 @@ import com.pousheng.middle.warehouse.service.WarehouseReadService;
 import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import io.terminus.common.exception.ServiceException;
 import io.terminus.common.model.Response;
+import io.terminus.common.utils.Arguments;
 import io.terminus.common.utils.Joiners;
 import io.terminus.common.utils.JsonMapper;
 import io.terminus.parana.shop.model.Shop;
@@ -30,6 +30,8 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by songrenfei on 2017/7/19
@@ -83,7 +85,7 @@ public class QueryHkWarhouseOrShopStockApi {
         List<HkSkuStockInfo> middleStockList =  Lists.newArrayListWithCapacity(hkSkuStockInfoList.size());
 
         for (HkSkuStockInfo skuStockInfo : hkSkuStockInfoList){
-            if(Objects.equal(2,Integer.valueOf(skuStockInfo.getStock_type()))){
+            if(Objects.equals(2,Integer.valueOf(skuStockInfo.getStock_type()))){
                 try {
                     Warehouse warehouse = warehouseCacher.findByCode(skuStockInfo.getCompany_id()+"-"+skuStockInfo.getStock_id());
                     skuStockInfo.setBusinessId(warehouse.getId());
@@ -97,16 +99,14 @@ public class QueryHkWarhouseOrShopStockApi {
                 try {
                     Shop shop = middleShopCacher.findByOuterIdAndBusinessId(skuStockInfo.getStock_code(),Long.valueOf(skuStockInfo.getCompany_id()));
                     //过滤掉已冻结或已删除的店铺
-                    if(!Objects.equal(shop.getStatus(),1)){
+                    if(!Objects.equals(shop.getStatus(),1)){
                         log.warn("current shop(id:{}) status:{} invalid,so skip",shop.getId(),shop.getStatus());
                         continue;
                     }
                     skuStockInfo.setBusinessId(shop.getId());
                     skuStockInfo.setBusinessName(shop.getName());
                     //商品必须打标为mpos标签才可以参与门店发货
-                    if(checkIsMposSku(skuStockInfo)){
-                        middleStockList.add(skuStockInfo);
-                    }
+                    filterIsMposSku(skuStockInfo,middleStockList);
 
                 }catch (Exception e){
                     log.error("find shop by outer id:{} fail,cause:{}",skuStockInfo.getStock_code(),Throwables.getStackTraceAsString(e));
@@ -119,10 +119,13 @@ public class QueryHkWarhouseOrShopStockApi {
     }
 
 
-    private Boolean checkIsMposSku(HkSkuStockInfo skuStockInfo){
+    //只有打标为mpos标签的货品才可以参与派单
+    private void filterIsMposSku(HkSkuStockInfo skuStockInfo,List<HkSkuStockInfo> middleStockList){
 
+        List<HkSkuStockInfo.SkuAndQuantityInfo> materialList = skuStockInfo.getMaterial_list();
+        List<HkSkuStockInfo.SkuAndQuantityInfo> newMaterialList = Lists.newArrayListWithCapacity(materialList.size());
 
-        List<String> skuCodes = Lists.transform(skuStockInfo.getMaterial_list(), new Function<HkSkuStockInfo.SkuAndQuantityInfo, String>() {
+        List<String> skuCodes = Lists.transform(materialList, new Function<HkSkuStockInfo.SkuAndQuantityInfo, String>() {
             @Nullable
             @Override
             public String apply(@Nullable HkSkuStockInfo.SkuAndQuantityInfo input) {
@@ -131,26 +134,35 @@ public class QueryHkWarhouseOrShopStockApi {
         });
 
         Response<List<SkuTemplate>> listRes = skuTemplateReadService.findBySkuCodes(skuCodes);
-        if(!listRes.isSuccess()){
+        if( !listRes.isSuccess()){
             log.error("find sku template by sku codes:{} fail,error:{}",skuCodes,listRes.getError());
             //这里将查询失败当做非mpos商品处理（严格意义上这里应该要报错的）
-            return Boolean.FALSE;
+            return;
         }
         List<SkuTemplate> skuTemplates = listRes.getResult();
 
         if (CollectionUtils.isEmpty(skuTemplates)){
             log.error("not find sku template by sku codes:{} ",skuCodes);
             //这里将查询不到的当做非mpos商品处理（严格意义上这里应该要报错的）
-            return Boolean.FALSE;
+            return ;
         }
 
-        for (SkuTemplate skuTemplate : skuTemplates){
-            if (!Objects.equal(skuTemplate.getType(),2)){
-                return Boolean.FALSE;
+        Map<String, SkuTemplate> skuInfoMap = skuTemplates.stream().filter(Objects::nonNull)
+                .collect(Collectors.toMap(SkuTemplate::getSkuCode, it -> it));
+
+        for (HkSkuStockInfo.SkuAndQuantityInfo skuAndQuantityInfo : materialList) {
+            SkuTemplate skuTemplate = skuInfoMap.get(skuAndQuantityInfo.getBarcode());
+            if (Arguments.isNull(skuTemplate)){
+                log.error("not find sku template by sku code:{}",skuAndQuantityInfo.getBarcode());
+                continue;
+            }
+            if (Objects.equals(skuTemplate.getType(),2)){
+                newMaterialList.add(skuAndQuantityInfo);
             }
         }
 
-        return Boolean.TRUE;
+        skuStockInfo.setMaterial_list(newMaterialList);
+        middleStockList.add(skuStockInfo);
 
     }
 

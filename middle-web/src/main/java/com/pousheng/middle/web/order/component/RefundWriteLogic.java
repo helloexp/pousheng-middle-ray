53ef1ba4db0f927ccdb9832b5c9c602479fba6c7
zhaoxiaotao <zhaoxiaotao@terminus.io>
2018-08-08 09:58:47 +0800

    退货退款按照金额传给恒康

--- middle-web/src/main/java/com/pousheng/middle/web/order/component/RefundWriteLogic.java
+++ middle-web/src/main/java/com/pousheng/middle/web/order/component/RefundWriteLogic.java
@@ -383,7 +383,7 @@ public class RefundWriteLogic {
                 extraMap.put(TradeConstants.MIDDLE_CHANGE_RECEIVE_INFO, mapper.toJson(submitRefundInfo.getMiddleChangeReceiveInfo()));
             }
         }
-        if (Objects.equals(submitRefundInfo.getRefundType(),MiddleRefundType.AFTER_SALES_CHANGE.value())||Objects.equals(submitRefundInfo.getRefundType(),MiddleRefundType.AFTER_SALES_RETURN.value())){
+        if (Objects.equals(submitRefundInfo.getRefundType(),MiddleRefundType.AFTER_SALES_CHANGE.value())){
             //换货的金额用商品净价*申请数量
             Long totalRefundAmount = 0L;
             for (RefundItem refundItem :refundItems){

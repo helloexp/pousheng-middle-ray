93e550cfe2545a734056acc0063f731094457fec
songrenfei <songrenfei@terminus.io>
2018-10-28 22:05:51 +0800

    update cancel jd order

--- middle-web/src/main/java/com/pousheng/middle/web/order/job/JdNotPaidOrderJob.java
+++ middle-web/src/main/java/com/pousheng/middle/web/order/job/JdNotPaidOrderJob.java
@@ -11,13 +11,14 @@ import com.pousheng.middle.web.order.component.OrderWriteLogic;
 import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import io.terminus.common.model.Paging;
 import io.terminus.common.model.Response;
-import io.terminus.common.utils.JsonMapper;
 import io.terminus.open.client.common.shop.model.OpenShop;
 import io.terminus.open.client.common.shop.service.OpenShopReadService;
 import io.terminus.open.client.jd.order.JdOrderService;
 import io.terminus.parana.order.model.ShopOrder;
 import io.terminus.parana.order.service.ShopOrderReadService;
+import io.terminus.zookeeper.leader.HostLeader;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -27,8 +28,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -54,8 +53,10 @@ public class JdNotPaidOrderJob{
     private MiddleOrderReadService middleOrderReadService;
     @RpcConsumer
     private OpenShopReadService openShopReadService;
+    @Autowired
+    private HostLeader hostLeader;
 
-    private static final JsonMapper mapper = JsonMapper.nonEmptyMapper();
+
 
     /**
      * 定时轮询取消04:00 16:00
@@ -63,6 +64,11 @@ public class JdNotPaidOrderJob{
     @Scheduled(cron = "0 0 4,16 * * ? *")
     public void orderCancelJob() {
 
+        if (!hostLeader.isLeader()) {
+            log.info("orderCancelJob current leader is:{}, skip", hostLeader.currentLeaderId());
+            return;
+        }
+        log.info("START JOB JdNotPaidOrderJob.orderCancelJob");
         // 京东渠道店铺
         Response<List<OpenShop>> findR = openShopReadService.findByChannel(MiddleChannel.JD.getValue());
         if (!findR.isSuccess()) {
@@ -70,60 +76,74 @@ public class JdNotPaidOrderJob{
             return;
         }
         List<OpenShop> openShopList = findR.getResult();
-        openShopList.stream().forEach(openShop -> {
-            Long openShopId = openShop.getId();
-            MiddleOrderCriteria middleOrderCriteria = new MiddleOrderCriteria();
-            middleOrderCriteria.setShopId(openShopId);
-            try {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                middleOrderCriteria.setOutCreatedStartAt(sdf.parse("2018-10-19"));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            Integer pageNo = 1;
-            Integer pageSize = 200;
-            while(true) {
-                middleOrderCriteria.setPageNo(pageNo);
-                middleOrderCriteria.setPageSize(pageSize);
-                Response<Paging<ShopOrder>> pagingRes =  middleOrderReadService.pagingShopOrder(middleOrderCriteria);
-                if(!pagingRes.isSuccess()){
-                    log.error("find order paging by middleOrderCriteria {} fail", mapper.toJson(middleOrderCriteria));
-                    break;
-                }
-                List<ShopOrder> shopOrders = pagingRes.getResult().getData();
-                if (CollectionUtils.isEmpty(shopOrders)) {
-                    break;
-                }
+        openShopList.forEach(openShop -> doCancelShopOrder(openShop.getId()));
 
-                shopOrders.stream().forEach(shopOrder -> {
-
-                    // 已取消的跳过
-                    if(Objects.equals(shopOrder.getStatus(),MiddleOrderStatus.CANCEL.getValue())) {
-                        return;
-                    }
-
-                    // 不是预售单的跳过
-                    Map<String,String> extra = shopOrder.getExtra();
-                    if(CollectionUtils.isEmpty(extra) ||  !extra.containsKey(TradeConstants.IS_STEP_ORDER)) {
-                        return;
-                    }
-
-                    Response<Boolean> response = jdOrderService.existOrder(openShopId, shopOrder.getOutId(), "TRADE_CANCELED", "orderId");
-
-                    if(!response.isSuccess()) {
-                        log.error("find cancel order detail fail by shopId {}, outId {}, error {}", openShopId, shopOrder.getOutId(), response.getError());
-                        return;
-                    }
-
-                    doCancel(shopOrder.getOutId());
-
-                    log.info("cancel order success by openShopId {}, outerOrderId {}", openShopId, shopOrder.getOutId());
-                });
-
-                pageNo++;
-            }
-        });
+        log.info("END JOB JdNotPaidOrderJob.orderCancelJob");
     }
+
+    /**
+     * 取消店铺订单
+     * @param openShopId 店铺id
+     */
+    private void doCancelShopOrder(Long openShopId){
+        MiddleOrderCriteria middleOrderCriteria = new MiddleOrderCriteria();
+        middleOrderCriteria.setShopId(openShopId);
+        middleOrderCriteria.setOutCreatedStartAt(new DateTime("2018-10-19").toDate());
+        Integer pageNo = 1;
+        Integer pageSize = 200;
+        boolean next = batchHandle(pageNo, pageSize,middleOrderCriteria);
+        while (next) {
+            pageNo ++;
+            next = batchHandle(pageNo, pageSize,middleOrderCriteria);
+        }
+
+    }
+
+    private boolean batchHandle(int pageNo, int pageSize,MiddleOrderCriteria middleOrderCriteria){
+
+            middleOrderCriteria.setPageNo(pageNo);
+            middleOrderCriteria.setPageSize(pageSize);
+            Response<Paging<ShopOrder>> pagingRes =  middleOrderReadService.pagingShopOrder(middleOrderCriteria);
+            if (!pagingRes.isSuccess()){
+                log.error("find order paging by middleOrderCriteria {} fail ,error:{}", middleOrderCriteria,pagingRes.getError());
+                return Boolean.FALSE;
+            }
+            List<ShopOrder> shopOrders = pagingRes.getResult().getData();
+
+            if (pagingRes.getResult().getTotal().equals(0L)  || CollectionUtils.isEmpty(shopOrders)) {
+                return Boolean.FALSE;
+            }
+
+            shopOrders.forEach(shopOrder -> {
+
+                // 已取消的跳过
+                if (Objects.equals(shopOrder.getStatus(),MiddleOrderStatus.CANCEL.getValue())) {
+                    return;
+                }
+
+                // 不是预售单的跳过
+                Map<String,String> extra = shopOrder.getExtra();
+                if (CollectionUtils.isEmpty(extra) ||  !extra.containsKey(TradeConstants.IS_STEP_ORDER)) {
+                    return;
+                }
+
+                Response<Boolean> response = jdOrderService.existOrder(middleOrderCriteria.getShopId(), shopOrder.getOutId(), "TRADE_CANCELED", "orderId");
+
+                if (!response.isSuccess()) {
+                    log.error("find cancel order detail fail by shopId {}, outId {}, error {}", middleOrderCriteria.getShopId(), shopOrder.getOutId(), response.getError());
+                    return;
+                }
+
+                doCancel(shopOrder);
+
+                log.info("cancel order success by openShopId {}, outerOrderId {}", middleOrderCriteria.getShopId(), shopOrder.getOutId());
+            });
+
+        int current = shopOrders.size();
+        return current == pageSize;  // 判断是否存在下一个要处理的批次
+
+    }
+
 
 
     /**
@@ -154,64 +174,17 @@ public class JdNotPaidOrderJob{
      */
     @RequestMapping(value = "/cancel/batch/order", method = RequestMethod.GET)
     public void batchCancelOrder(@RequestParam(value = "openShopId") Long openShopId) {
-        MiddleOrderCriteria middleOrderCriteria = new MiddleOrderCriteria();
-        middleOrderCriteria.setShopId(openShopId);
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            middleOrderCriteria.setOutCreatedStartAt(sdf.parse("2018-10-19"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Integer pageNo = 1;
-        Integer pageSize = 200;
-        while(true) {
-            middleOrderCriteria.setPageNo(pageNo);
-            middleOrderCriteria.setPageSize(pageSize);
-            Response<Paging<ShopOrder>> pagingRes = middleOrderReadService.pagingShopOrder(middleOrderCriteria);
-            if (!pagingRes.isSuccess()) {
-                log.error("find order paging by middleOrderCriteria {} fail", mapper.toJson(middleOrderCriteria));
-                break;
-            }
-            List<ShopOrder> shopOrders = pagingRes.getResult().getData();
-            if (CollectionUtils.isEmpty(shopOrders)) {
-                break;
-            }
-//
-            shopOrders.stream().forEach(shopOrder -> {
 
-                // 已取消的跳过
-                if(Objects.equals(shopOrder.getStatus(),MiddleOrderStatus.CANCEL.getValue())) {
-                    return;
-                }
-
-                // 不是预售单的跳过
-                Map<String,String> extra = shopOrder.getExtra();
-                if(CollectionUtils.isEmpty(extra) ||  !extra.containsKey(TradeConstants.IS_STEP_ORDER)) {
-                    return;
-                }
-
-                Response<Boolean> response = jdOrderService.existOrder(openShopId, shopOrder.getOutId(), "TRADE_CANCELED", "orderId");
-
-                if(!response.isSuccess()) {
-                    log.error("find batch cancel order detail fail by shopId {}, outId {}, error {}", openShopId, shopOrder.getOutId(), response.getError());
-                    return;
-                }
-
-                doCancel(shopOrder.getOutId());
-
-                log.info("cancel batch order success by openShopId {}, outerOrderId {}", openShopId, shopOrder.getOutId());
-            });
-            pageNo++;
-        }
-
+        this.doCancelShopOrder(openShopId);
     }
+
 
 
     /**
      * 中台取消逻辑
-     * @param outerOrderId
+     * @param outerOrderId 外部单号
      */
-    public void doCancel(String outerOrderId) {
+    private void doCancel(String outerOrderId) {
         Response<Optional<ShopOrder>> shopOrderResponse = shopOrderReadService.findByOutIdAndOutFrom(outerOrderId,MiddleChannel.JD.getValue());
         if (!shopOrderResponse.isSuccess()) {
             log.error("find order fail by outerOrderId {}, error {}", outerOrderId, shopOrderResponse.getError());
@@ -223,16 +196,27 @@ public class JdNotPaidOrderJob{
             log.error("find order fail by outerOrderId {}", outerOrderId);
             return;
         }
-        ShopOrder shopOrder = shopOrderResponse.getResult().get();
+        ShopOrder shopOrder = shopOrderOptional.get();
+        doCancel(shopOrder);
+
+    }
+
+
+    /**
+     * 中台取消逻辑
+     * @param shopOrder 订单
+     */
+    private void doCancel(ShopOrder shopOrder) {
 
         // 逆向取消
         try {
             orderWriteLogic.autoCancelShopOrder(shopOrder.getId());
         } catch (Exception e) {
-            log.error("auto cancel jd shop order {} fail, error {} ", shopOrder.getId(), Throwables.getStackTraceAsString(e));
-            return;
+            log.error("auto cancel jd shop order {} fail, cause {} ", shopOrder.getId(), Throwables.getStackTraceAsString(e));
         }
+
     }
+
 
 
 }

c6998eba2e74762551ce44a3091b7e8ef5826153
zhaoxiaotao <zhaoxiaotao@terminus.io>
2018-10-31 12:10:39 +0800

    rocketmq 错误代码修复

--- middle-web/src/main/java/com/pousheng/middle/mq/consumer/CompensateCommonBizConsumerMessageListener.java
+++ middle-web/src/main/java/com/pousheng/middle/mq/consumer/CompensateCommonBizConsumerMessageListener.java
@@ -2,9 +2,9 @@ package com.pousheng.middle.mq.consumer;
 
 import com.pousheng.middle.mq.component.CompensateBizLogic;
 import com.pousheng.middle.mq.constant.MqConstants;
+import io.terminus.common.rocketmq.annotation.ConsumeMode;
+import io.terminus.common.rocketmq.annotation.MQSubscribe;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.starter.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.starter.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -13,17 +13,17 @@ import org.springframework.stereotype.Service;
 /**
  * 通用业务处理，16个线程
  * CompenSateBiz mq处理
+ * @author tony
  */
 @ConditionalOnProperty(name = "biz.common.topic.enable", havingValue = "true", matchIfMissing = true)
 @Slf4j
 @Service
-@RocketMQMessageListener(topic = MqConstants.POSHENG_MIDDLE_COMMON_COMPENSATE_BIZ_TOPIC,
-        consumerGroup = MqConstants.POUSHENG_MIDDLE_MQ_COMMON_CONSUMER_GROUP,consumeThreadMax = 16)
-public class CompensateCommonBizConsumerMessageListener implements RocketMQListener<String> {
+public class CompensateCommonBizConsumerMessageListener {
     @Autowired
     private CompensateBizLogic compensateBizLogic;
 
-    @Override
+    @MQSubscribe(topic = MqConstants.POSHENG_MIDDLE_COMMON_COMPENSATE_BIZ_TOPIC, consumerGroup = MqConstants.POUSHENG_MIDDLE_MQ_COMMON_CONSUMER_GROUP,
+            consumeMode = ConsumeMode.CONCURRENTLY)
     public void onMessage(String message) {
         if (log.isDebugEnabled()){
             log.debug("CompensateCommonBizConsumerMessageListener onMessage,message {}",message);
--- middle-web/src/main/java/com/pousheng/middle/mq/producer/RocketMqProducerService.java
+++ middle-web/src/main/java/com/pousheng/middle/mq/producer/RocketMqProducerService.java
@@ -1,17 +1,12 @@
 package com.pousheng.middle.mq.producer;
 
-import com.alibaba.fastjson.JSON;
 import com.google.common.base.Throwables;
+import io.terminus.common.rocketmq.core.TerminusMQProducer;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
-
-import org.apache.rocketmq.spring.starter.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.nio.charset.Charset;
 
 /**
  * @author 赵小涛
@@ -19,105 +14,46 @@ import java.nio.charset.Charset;
 @Slf4j
 @Service
 public class RocketMqProducerService {
-    @Autowired(required = false)
-    private RocketMQTemplate rocketMQTemplate;
+    @Autowired
+    private TerminusMQProducer producer;
 
     /**
      * 立即发送MQ消息
+     *
      * @param message messge必须是json字符串
      */
     public void sendMessage(String topic, String message) {
-        if (rocketMQTemplate == null) {
-            log.error("Error of sending message to MQ: no configure here, message: {}",message);
+        if (producer == null) {
+            log.error("Error of sending message to MQ: no configure here, message: {}", message);
             return;
         }
 
         // 开始发送消息
         try {
             if (log.isDebugEnabled()) {
-                log.debug("Sending message to MQ: {}",message);
+                log.debug("Sending message to MQ: {}", message);
             }
 
-            byte[] bytes = message.getBytes(Charset.forName("UTF-8"));
-
-            DefaultMQProducer producer = rocketMQTemplate.getProducer();
-            org.apache.rocketmq.common.message.Message msg =
-                    new org.apache.rocketmq.common.message.Message(topic, "", bytes);
             //消息异步发送
-            SendCallback sendCallback  = new SendCallback() {
+            SendCallback sendCallback = new SendCallback() {
                 @Override
                 public void onSuccess(SendResult sendResult) {
                     //必须打印msgId用来以备查验
-                    log.info("Sending message to MQ: {},msgId {}",message,sendResult.getMsgId());
+                    log.info("Sending message to MQ: {},msgId {}", message, sendResult.getMsgId());
                 }
 
                 @Override
                 public void onException(Throwable e) {
-                    log.error("ending message to MQ failed,msg {}",message);
+                    log.error("ending message to MQ failed,msg {}", message);
                 }
             };
             //消息发送
-            producer.send(msg,sendCallback,(long)producer.getSendMsgTimeout());
+            producer.asyncSend(topic, message, sendCallback);
 
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             log.error("Error of send message to MQ! message: {}, stackTrace: {}",
                     message, Throwables.getStackTraceAsString(ex));
         }
     }
 
-    /**
-     * 尝试发送RocketMQ的延迟消息，如果level为空，那么将立即发送消息
-     * @param message message必须是json字符串
-     * @param level
-     */
-    public void sendMessage(String topic, String message, MessageLevel level) {
-        if (level == null) {
-            if (log.isDebugEnabled()) {
-                log.debug("No messageLevel, choose normal channel...");
-            }
-
-            this.sendMessage(topic, message);
-            return;
-        }
-
-        if (rocketMQTemplate == null) {
-            log.error("Error of sending message to MQ: no configure here, message: {}", message);
-            return;
-        }
-
-        try {
-            long now = System.currentTimeMillis();
-            byte[] bytes = message.getBytes(Charset.forName("UTF-8"));
-
-            DefaultMQProducer producer = rocketMQTemplate.getProducer();
-            org.apache.rocketmq.common.message.Message msg =
-                    new org.apache.rocketmq.common.message.Message(topic, "", bytes);
-
-            // 设置延迟级别
-            msg.setDelayTimeLevel(level.resolve());
-            //消息异步发送
-            SendCallback sendCallback  = new SendCallback() {
-                @Override
-                public void onSuccess(SendResult sendResult) {
-                    //必须打印msgId用来以备查验
-                    log.info("Sending message to MQ: {},msgId {}",message,sendResult.getMsgId());
-                }
-                @Override
-                public void onException(Throwable e) {
-                    log.error("ending message to MQ failed,msg {}",message);
-                }
-            };
-
-            producer.send(msg,sendCallback, (long)producer.getSendMsgTimeout());
-
-            long costTime = System.currentTimeMillis() - now;
-
-
-        } catch (Exception ex) {
-            log.error("Error of send message to MQ! message: {}, stackTrace: {}",
-                    JSON.toJSON(message), Throwables.getStackTraceAsString(ex));
-        }
-
-    }
 }

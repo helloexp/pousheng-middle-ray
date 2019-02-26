55917b09d3f9fac97a91d5e9362c52b62fa2e9ed
Warren Chen <warren.chen@pousheng.com>
2019-02-26 17:53:48 +0800

    //门店扫码订单归属國瑞城                 posContent.setCompanyid("200");//绩效店铺所属公司id                 posContent.setShopcode("SP110073");//绩效店铺code

--- middle-web/src/main/java/com/pousheng/middle/web/order/sync/hk/SyncShipmentPosLogic.java
+++ middle-web/src/main/java/com/pousheng/middle/web/order/sync/hk/SyncShipmentPosLogic.java
@@ -247,12 +247,12 @@ public class SyncShipmentPosLogic {
             }
 
             //门店扫码订单归属國瑞城
-            // Map<String, String> shopOrderExtra = shopOrder.getExtra();
-            // String isStoreScanCode = shopOrderExtra.get("is_store_scan_code");
-            // if (!StringUtils.isEmpty(isStoreScanCode) && Objects.equal(isStoreScanCode, "true")) {  //门店扫码订单
-            //     posContent.setCompanyid(companyId);//绩效店铺所属公司id
-            //     posContent.setShopcode(code);//绩效店铺code
-            // }
+            Map<String, String> shopOrderExtra = shopOrder.getExtra();
+            String isStoreScanCode = shopOrderExtra.get("is_store_scan_code");
+            if (!StringUtils.isEmpty(isStoreScanCode) && Objects.equal(isStoreScanCode, "true")) {  //门店扫码订单
+                posContent.setCompanyid("200");//绩效店铺所属公司id
+                posContent.setShopcode("SP110073");//绩效店铺code
+            }
         }
         posContent.setVoidstockcode(posStockCode);//todo 实际发货账套的虚拟仓代码
 

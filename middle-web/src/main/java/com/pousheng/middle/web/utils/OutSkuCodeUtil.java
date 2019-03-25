7c56a8def3504d960c29e0d24761ed39000830c4
James Bacon <jambecome@gmail.com>
2019-03-25 20:26:51 +0800

    修复售后单 skucode 相同的多条子单只生成了一条合并记录的问题

--- middle-web/src/main/java/com/pousheng/middle/web/utils/OutSkuCodeUtil.java
+++ middle-web/src/main/java/com/pousheng/middle/web/utils/OutSkuCodeUtil.java
@@ -19,7 +19,7 @@ public class OutSkuCodeUtil {
      * @param refundItem
      * @return
      */
-    public static final String getRefundItemComplexSkuCode(RefundItem refundItem) {
+    public static final String getCombineCode(RefundItem refundItem) {
         String skuOrderId = refundItem.getSkuOrderId() != null ? refundItem.getSkuOrderId().toString() : EMPTY_STR;
         String skuCode = MoreObjects.firstNonNull(refundItem.getSkuCode(),  EMPTY_STR);
         String outSkuCode = MoreObjects.firstNonNull(refundItem.getOutSkuCode(), EMPTY_STR);
@@ -43,7 +43,7 @@ public class OutSkuCodeUtil {
      * @param editSubmitRefundItem
      * @return
      */
-    public static final String getEditSubmitRefundItemComplexSkuCode(EditSubmitRefundItem editSubmitRefundItem) {
+    public static final String getCombineCode(EditSubmitRefundItem editSubmitRefundItem) {
         String skuOrderId = editSubmitRefundItem.getSkuOrderId() != null
                 ? editSubmitRefundItem.getSkuOrderId().toString() : EMPTY_STR;
         String skuCode = editSubmitRefundItem.getRefundSkuCode();
